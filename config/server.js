627ebab0df7cc4cfa7e62f45c699ce5ca5fd6fc4
PrinceAbaidoo <prince.abaidoo@amalitech.com>
2022-04-23 08:54:25 +0000

    update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  url: 'http://localhost',
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
