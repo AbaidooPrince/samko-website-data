8525319ec6a99680c464a7a3b03ec91fa76341d9
PrinceAbaidoo <prince.abaidoo@amalitech.com>
2022-04-23 12:40:38 +0000

    added about section

---
+++ src/api/morden-section/routes/morden-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * morden-section router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::morden-section.morden-section');
---
+++ src/api/morden-section/services/morden-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * morden-section service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::morden-section.morden-section');
